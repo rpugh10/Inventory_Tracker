fba55807a891f782a8c66f61d5a747eeecc949de
Ryan Pugh <rpugh777@yahoo.com>
2026-08-24 19:03:39 -0700

    Product Service

--- src/main/java/com/example/inventoryTracker/Service/ProductService.java
+++ src/main/java/com/example/inventoryTracker/Service/ProductService.java
@@ -2,14 +2,28 @@ package com.example.inventoryTracker.Service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.inventoryTracker.DTO.ProductDTO;
+import com.example.inventoryTracker.Mapper.ProductMapper;
 import com.example.inventoryTracker.Repository.ProductRepository;
 
 @Service
 public class ProductService {
     
     private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
         this.productRepository = productRepository;
+        this.productMapper = productMapper;
     }
+
+    public ProductDTO findProductById(Long id) {
+        return productRepository.findById(id)
+                .map(productMapper::toProductDTO)
+                .orElseGet(() -> {
+                    throw new RuntimeException("Product not found with id: " + id);
+                });
+    }
+
+    
 }
