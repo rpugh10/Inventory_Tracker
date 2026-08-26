7ff4d28cbad8a9a40d3fb2c57b6103692c4606fe
Ryan Pugh <rpugh777@yahoo.com>
2026-08-25 19:38:58 -0700

    Working product service

--- src/main/java/com/example/inventoryTracker/Service/ProductService.java
+++ src/main/java/com/example/inventoryTracker/Service/ProductService.java
@@ -1,8 +1,13 @@
 package com.example.inventoryTracker.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.example.inventoryTracker.DTO.ProductDTO;
+import com.example.inventoryTracker.Entities.Product;
 import com.example.inventoryTracker.Mapper.ProductMapper;
 import com.example.inventoryTracker.Repository.ProductRepository;
 
@@ -18,11 +23,49 @@ public class ProductService {
     }
 
     public ProductDTO findProductById(Long id) {
-        return productRepository.findById(id)
+        return productRepository.findById(id) //Don't need to use .stream() because .findById returns an optional.
                 .map(productMapper::toProductDTO)
                 .orElseGet(() -> {
                     throw new RuntimeException("Product not found with id: " + id);
                 });
+    }
+
+    public List<ProductDTO> findAllProducts() {
+        return productRepository.findAll().stream() //Have to use .stream() because .findAll() returns a List
+                .map(productMapper::toProductDTO)
+                .collect(Collectors.toList());
+    }
+
+    public ProductDTO saveProduct(ProductDTO productDTO) {
+        Product product = productMapper.toProduct(productDTO);
+        Product savedProduct = productRepository.save(product);
+        return productMapper.toProductDTO(savedProduct);
+        
+    }
+
+    public ProductDTO updateProduct(Long id, ProductDTO productDTO) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product existingProduct = optionalProduct.orElseThrow(() -> new RuntimeException("Product not found with id: " + id));
+            existingProduct.setProductName(productDTO.getProductName());
+            existingProduct.setUnit(productDTO.getUnit());
+            existingProduct.setSku(productDTO.getSku());
+            existingProduct.setReorderLevel(productDTO.getReorderLevel());
+            existingProduct.setUnitCost(productDTO.getUnitCost());
+            Product updatedProduct = productRepository.save(existingProduct);
+            return productMapper.toProductDTO(updatedProduct);
+        } else {
+            throw new RuntimeException("Product not found with id: " + id);
+        }
+    }
+
+    public void deleteProduct(Long id){
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            productRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Product not found with id: " + id);
+        }
     }
 
     
