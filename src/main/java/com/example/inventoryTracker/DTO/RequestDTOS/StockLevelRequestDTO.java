3c210355d094cd043c2cc025a367628818ff998d
Ryan Pugh <rpugh777@yahoo.com>
2026-08-28 19:17:32 -0700

    SL Request DTO

---
+++ src/main/java/com/example/inventoryTracker/DTO/RequestDTOS/StockLevelRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.inventoryTracker.DTO.RequestDTOS;
+
+import com.example.inventoryTracker.Entities.TransactionType;
+
+import lombok.Data;
+
+@Data
+public class StockLevelRequestDTO {
+
+    private Long productId;
+    private Long locationId;
+    private Integer quantity;
+    private TransactionType transactionType;
+}
