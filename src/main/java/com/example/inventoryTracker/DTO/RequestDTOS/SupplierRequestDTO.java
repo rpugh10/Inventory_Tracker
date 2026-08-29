226d7451890fa166f134fa2f613ff953f06fe552
Ryan Pugh <rpugh777@yahoo.com>
2026-08-28 19:18:03 -0700

    SR DTO

---
+++ src/main/java/com/example/inventoryTracker/DTO/RequestDTOS/SupplierRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.inventoryTracker.DTO.RequestDTOS;
+
+import lombok.Data;
+
+@Data
+public class SupplierRequestDTO {
+    
+    
+    private String supplierName;
+    private String email;
+    private String phoneNumber;
+}
