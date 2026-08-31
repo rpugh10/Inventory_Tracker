5c33ec6d48d7b9c8d3ba5798d6c315c7e29dfe7d
Ryan Pugh <rpugh777@yahoo.com>
2026-08-30 19:25:12 -0700

    Added Customer User Service

---
+++ src/main/java/com/example/inventoryTracker/Service/CustomerUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.inventoryTracker.Service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.inventoryTracker.Repository.AppUserRepository;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final AppUserRepository userRepository;
+
+    public CustomerUserDetailsService(AppUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+
+}
