08e46673468e4739a26a83a2ea65199ea72613e0
Ryan Pugh <rpugh777@yahoo.com>
2026-08-30 19:24:33 -0700

    Added a find by username method

--- src/main/java/com/example/inventoryTracker/Repository/AppUserRepository.java
+++ src/main/java/com/example/inventoryTracker/Repository/AppUserRepository.java
@@ -1,9 +1,11 @@
 package com.example.inventoryTracker.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.inventoryTracker.Entities.AppUser;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-
+    Optional<AppUser> findByUsername(String username);
 }
