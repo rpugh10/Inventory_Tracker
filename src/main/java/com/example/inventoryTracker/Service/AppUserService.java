4aeeaf2f2aac7f804fd111e76fb9a3e561280718
Ryan Pugh <rpugh777@yahoo.com>
2026-08-30 22:10:56 -0700

    Removed an unused import

--- src/main/java/com/example/inventoryTracker/Service/AppUserService.java
+++ src/main/java/com/example/inventoryTracker/Service/AppUserService.java
@@ -3,7 +3,7 @@ package com.example.inventoryTracker.Service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
