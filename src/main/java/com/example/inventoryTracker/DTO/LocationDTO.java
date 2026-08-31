0ba106f5a9985cf4824a5974e0add8791c7dc69b
Ryan Pugh <rpugh777@yahoo.com>
2026-08-30 20:05:55 -0700

    Created a PasswordRequestDTO

--- src/main/java/com/example/inventoryTracker/DTO/LocationDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.inventoryTracker.DTO;
-
-import lombok.Data;
-
-@Data
-public class LocationDTO {
-
-    private String locationName;
-}
---
+++ src/main/java/com/example/inventoryTracker/DTO/RequestDTOS/PasswordRequestDTO.java
@@ -0,0 +1,8 @@
+package com.example.inventoryTracker.DTO.RequestDTOS;
+
+import lombok.Data;
+
+@Data
+public class PasswordRequestDTO {
+    private String newPassword;
+}
