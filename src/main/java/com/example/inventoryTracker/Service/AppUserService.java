d394c0b8b921d14d303c2d2752403f0d37169028
Ryan Pugh <rpugh777@yahoo.com>
2026-08-30 20:05:19 -0700

    Separted the update user info and user password

--- src/main/java/com/example/inventoryTracker/Service/AppUserService.java
+++ src/main/java/com/example/inventoryTracker/Service/AppUserService.java
@@ -3,6 +3,8 @@ package com.example.inventoryTracker.Service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.inventoryTracker.DTO.RequestDTOS.AppUserRequestDTO;
@@ -16,10 +18,12 @@ public class AppUserService {
 
     private final AppUserMapper appUserMapper;
     private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AppUserService(AppUserMapper appUserMapper, AppUserRepository appUserRepository) {
+    public AppUserService(AppUserMapper appUserMapper, AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
         this.appUserMapper = appUserMapper;
         this.appUserRepository = appUserRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public AppUserResponseDTO getUserById(Long id){
@@ -36,15 +40,15 @@ public class AppUserService {
 
     public AppUserResponseDTO createUser(AppUserRequestDTO userDTO){
         AppUser user = appUserMapper.toAppUser(userDTO);
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         AppUser savedUser = appUserRepository.save(user);
         return appUserMapper.toAppUserDTO(savedUser);
     }
 
-    public AppUserResponseDTO updateUser(Long id, AppUserRequestDTO userDTO){
+    public AppUserResponseDTO updateUserInformation(Long id, AppUserRequestDTO userDTO){
         Optional<AppUser> user = appUserRepository.findById(id); //Here we have to do Optional<AppUser> because findById() returns that.
         AppUser newUser = user.orElseThrow(() -> new RuntimeException("User not found"));
         newUser.setUsername(userDTO.getUsername());
-        newUser.setPassword(userDTO.getPassword());
         newUser.setEmail(userDTO.getEmail());
         newUser.setRole(userDTO.getRole());
         AppUser updatedUser = appUserRepository.save(newUser);
@@ -52,12 +56,20 @@ public class AppUserService {
          
     }
 
+    public AppUserResponseDTO updatePassword(Long id, String newPassword){
+        Optional<AppUser> user = appUserRepository.findById(id);
+        AppUser existingUser = user.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        existingUser.setPassword(passwordEncoder.encode(newPassword));
+        AppUser updatedUser = appUserRepository.save(existingUser);
+        return appUserMapper.toAppUserDTO(updatedUser);
+    }
+
     public void deleteUser(Long id){
         Optional<AppUser> user = appUserRepository.findById(id);
         if (user.isPresent()) {
             appUserRepository.deleteById(id);
         } else {
-            throw new RuntimeException("User not found with id: " + id);
+            throw new UsernameNotFoundException("User not found with id: " + id);
         }
     }
 }
