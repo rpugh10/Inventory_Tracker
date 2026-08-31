bda0e05998fbc7a16d0bd5fd144eafb7effbf333
Ryan Pugh <rpugh777@yahoo.com>
2026-08-30 22:04:30 -0700

    Added A PasswordEncoder Bean

--- src/main/java/com/example/inventoryTracker/Config/SecurityConfig.java
+++ src/main/java/com/example/inventoryTracker/Config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.inventoryTracker.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -21,7 +23,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
          http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/home", "/public/**").permitAll() 
+                .requestMatchers("/users/**", "/home", "/public/**").permitAll() 
                 .anyRequest().authenticated()                           
             )
             .formLogin(form -> form
