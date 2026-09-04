005968a4f90e85bd41243715e6c12ed0bfa9e34e
Ryan Pugh <rpugh777@yahoo.com>
2026-09-04 13:36:27 -0700

    Created an AuthenticationManager Bean and uppdated what endpoints didn't need validation

--- src/main/java/com/example/inventoryTracker/Config/SecurityConfig.java
+++ src/main/java/com/example/inventoryTracker/Config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.example.inventoryTracker.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.inventoryTracker.Utility.JwtAuthenticationFilter;
 
 
 @Configuration
@@ -20,18 +26,20 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
          http
+            .csrf(csrf -> csrf.disable()) // Disable CSRF protection
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // Add the JWT authentication filter before the UsernamePasswordAuthenticationFilter
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/users/**", "/home", "/public/**").permitAll() 
+                .requestMatchers("/login", "/register").permitAll() // Allow unauthenticated access to the login and register endpoints
+                .requestMatchers(HttpMethod.DELETE,"/users/**").hasRole("ADMIN") // Allow only users with the ADMIN role to access endpoints under /users/**
                 .anyRequest().authenticated()                           
-            )
-            .formLogin(form -> form
-                .loginPage("/login")                                   
-                .permitAll()
-            )
-            .logout(logout -> logout.permitAll());
-
+            );
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
