c5641e9cfb6513de31cfaa812ec13bec2800adf9
Ryan Pugh <rpugh777@yahoo.com>
2026-09-21 18:58:28 -0700

    Added validation

--- src/main/java/com/example/inventoryTracker/DTO/RequestDTOS/TransactionRequestDTOS/InventoryTransactionRequestDTO.java
+++ src/main/java/com/example/inventoryTracker/DTO/RequestDTOS/TransactionRequestDTOS/InventoryTransactionRequestDTO.java
@@ -2,15 +2,32 @@ package com.example.inventoryTracker.DTO.RequestDTOS.TransactionRequestDTOS;
 
 import com.example.inventoryTracker.Entities.Enums.TransactionType;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class InventoryTransactionRequestDTO {
 
+    @NotNull 
     private Long AppUserId;
+
+    
     private Long supplierId;
+
+    @NotNull 
     private Long locationId;
+
+    @NotNull 
     private Long productId;
+
+    
+    private String note;
+
+    @NotNull 
+    @Positive 
     private Integer quantity;
+
+    @NotNull 
     private TransactionType transactionTypeEnum;
 }
