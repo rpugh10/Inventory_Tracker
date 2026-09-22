e2b0ed75f988a090210f276322b3196b9f9699e1
Ryan Pugh <rpugh777@yahoo.com>
2026-09-21 19:02:16 -0700

    Added Custom exception methods

--- src/main/java/com/example/inventoryTracker/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/inventoryTracker/ExceptionHandler/GlobalExceptionHandler.java
@@ -6,7 +6,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.inventoryTracker.ExceptionHandler.Exceptions.DuplicateUser;
+import com.example.inventoryTracker.ExceptionHandler.Exceptions.InvalidQuantity;
+import com.example.inventoryTracker.ExceptionHandler.Exceptions.InvalidTransactionType;
+import com.example.inventoryTracker.ExceptionHandler.Exceptions.InventoryTransactionIdNotFound;
+import com.example.inventoryTracker.ExceptionHandler.Exceptions.LocationNotFound;
+import com.example.inventoryTracker.ExceptionHandler.Exceptions.ProductNotFound;
+import com.example.inventoryTracker.ExceptionHandler.Exceptions.StockLevelIdNotFound;
+import com.example.inventoryTracker.ExceptionHandler.Exceptions.SupplierNotFound;
 import com.example.inventoryTracker.ExceptionHandler.Exceptions.UserNotFoundException;
+import com.example.inventoryTracker.ExceptionHandler.Exceptions.InsufficientStock;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,4 +29,43 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
+    @ExceptionHandler(ProductNotFound.class)
+    public ResponseEntity<String> handleProductNotFound(ProductNotFound ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler (LocationNotFound.class)
+    public ResponseEntity<String> handleLocationNotFound(LocationNotFound ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler (SupplierNotFound.class)
+    public ResponseEntity<String> handleSupplierNotFound(SupplierNotFound ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler (InvalidQuantity.class)
+    public ResponseEntity<String> handleInvalidQuantity(InvalidQuantity ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler (InsufficientStock.class)
+    public ResponseEntity<String> handleInsufficientStock(InsufficientStock ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler (InventoryTransactionIdNotFound.class)
+    public ResponseEntity<String> handleInventoryTransactionIdNotFound(InventoryTransactionIdNotFound ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler (StockLevelIdNotFound.class)
+    public ResponseEntity<String> handleStockLevelIdNotFound(StockLevelIdNotFound ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler (InvalidTransactionType.class)
+    public ResponseEntity<String> handleInvalidTransactionType(InvalidTransactionType ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
 }
